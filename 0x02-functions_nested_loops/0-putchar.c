dd970fb402f2f21701c992816d859e322c80824e
Ayoub <sb.naba2@gmail.com>
2023-08-15 08:02:47 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char rang[] = "_putchar";
+	int i;
+
+	for (i = 0; rang[i] != '\0'; i++)
+	{
+		putchar(rang[i]);
+	}
+	putchar('\n');
+	return (0);
+}
