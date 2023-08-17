669bc0284034d742ddb64a6d6d54d2bf2d59ce93
Ayoub <sb.naba2@gmail.com>
2023-08-17 11:56:35 -0700

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,13 +11,13 @@ int main(void)
 	long int i = 3, n;
 
 	n = 612852475143;
-	while (i < n)
+	while (i <= n)
 	{
 		if ((n % i) != 0)
 			i += 2;
 		else
 			n /= i;
 	}
-	printf("%ld", n);
+	printf("%ld", i);
 	return (0);
 }
