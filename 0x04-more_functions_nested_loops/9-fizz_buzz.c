f9f988ffcd612cb2145a32a3e07cbafde9909d48
Ayoub <sb.naba2@gmail.com>
2023-08-17 09:21:20 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - entry point
  * Return: 0
@@ -7,34 +8,18 @@ int main(void)
 {
 	int i;
 
-	_putchar('1');
+	printf("1");
 	for (i = 2; i <= 100; i++)
-	{	_putchar(' ');
+	{
 		if ((i % 3) != 0 && (i % 5) != 0)
-			_putchar(i + '0');
+			printf(" %d", i);
 		if ((i % 3) == 0 && (i % 5) != 0)
-		{	_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf(" Fizz");
 		if ((i % 3) != 0 && (i % 5) == 0)
-		{	_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf(" Buzz");
 		if ((i % 3) == 0 && (i % 5) == 0)
-		{	_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
+			printf(" FizzBuzz");
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
 }
