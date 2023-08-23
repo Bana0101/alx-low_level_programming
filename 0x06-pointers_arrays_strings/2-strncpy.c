144baa780a813903b9f8ac7d7eece20b2e4b9c9e
Ayoub <sb.naba2@gmail.com>
2023-08-23 15:24:06 -0700

    copy a string into another for n character

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	do {
+	for (i = 0; (src[i] != '\0') && (i < n); i++)
 		dest[i] = src[i];
-		i++;
-	} while ((src[i] != '\0') && (i < n));
 	while (i < n)
 	{
 		dest[i] = '\0';
