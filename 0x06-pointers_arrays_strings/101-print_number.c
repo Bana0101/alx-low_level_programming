32f56345533f088254672c672df6172826fad898
Ayoub <sb.naba2@gmail.com>
2023-08-24 10:47:50 -0700

    a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,15 @@
 
 void print_number(int n)
 {
+	unsigned int deep;
+
+	deep = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		deep = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (deep / 10)
+		print_number(deep / 10);
+	_putchar((deep % 10) + '0');
 }
