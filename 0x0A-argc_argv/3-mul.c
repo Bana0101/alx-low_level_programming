c9bc3f1c416312f058dd150a908d5240b02c3811
Ayoub <sb.naba2@gmail.com>
2023-09-01 10:55:37 -0700

    a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _ato - a function that convert a string to an integer.
+ * @s: input adress
+ * Return: v
+*/
+
+int _ato(char *s)
+{
+	int sign = 1, i = 0;
+	unsigned int v = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+			sign = -sign;
+		if (s[i] <= 57 && s[i] >= 48)
+		{
+			v = v * 10 + (s[i] - '0');
+			if (s[i + 1] > 57 || s[i + 1] < 48)
+				break;
+		}
+		i++;
+	}
+	return (sign * v);
+
+}
+/**
+ * main - entry point
+ * @argc: the number of arguments
+ * @argv: arguments value
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int i = _ato(argv[1]), j = _ato(argv[2]);
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", i * j);
+	}
+	return (0);
+}
