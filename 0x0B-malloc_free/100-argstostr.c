3f77c756b445f26f95ab4d52d1a864cdf9cfbaa3
Ayoub <sb.naba2@gmail.com>
2023-09-05 06:54:58 -0700

    concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 		for (j = 0; av[i][j]; count++, j++)
 			count++;
-	grid = malloc(count * sizeof(grid) + 1);
+	grid = malloc(count + 1);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
