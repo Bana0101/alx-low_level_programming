2b4560f74fb74a0ce74688088706dde694ba2a4b
Ayoub <sb.naba2@gmail.com>
2023-09-05 13:28:17 -0700

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,29 +9,29 @@
 char **strtow(char *str)
 
 {
-	int i = 0, j, width = 0, line = 0, nline = 1;
+	int i = 0, j, width = 0, line = 0, nline = 0;
 	char **grid;
 
-	if (str[i] == '\0')
+	if (str[i] == '\0' || str == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		if (str[i] == ' ' &&  str[i + 1] != ' ' && str[i + 1] != '\0')
 			nline++;
-		else if (i == 0)
+		else if (i == 0 && str[i] != ' ' )
 			nline++;
 	}
-	if (nline)
+	if (nline == 0)
 		return (NULL);
 	grid = (char **)malloc(nline * sizeof(char) + 1);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			width = 0;
-			for (j = 1; str[i + j] != ' ' && str[i + j] != '\0'; j++)
+			for (j = 0; str[i + j] != ' ' && str[i + j] != '\0'; j++)
 				width++;
 			width++;
 			grid[line] = (char *)malloc(sizeof(char) * width);
@@ -42,10 +42,11 @@ char **strtow(char *str)
 				free(grid);
 				return (NULL);
 			}
-			for (j = 1; j < width; j++)
-				grid[line][j - 1] = str[i + j];
+			for (j = 0; j < width; j++)
+				grid[line][j] = str[i + j];
 			grid[line][width] = '\0';
 			line++;
+			i += width - 1;
 		}
 	}
 	return (grid);
