282dbf808cdbcf21225c49f6f1feaa223b22f06a
Ayoub <sb.naba2@gmail.com>
2023-09-07 15:27:56 -0700

    a function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _set - function set memory to zero
+ * @s: input char
+ * @b: input character
+ * @n: input integer
+ * Return: s
+ */
+
+char _set(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
  * *_calloc - allocates memory for an array, using malloc.
  * @nmemb: number of elements
  * @size: the size of elements
@@ -18,9 +35,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(nmemb * sizeof(int));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < tbytes; i++)
-	{
-		s[i] = '0';
-	}
+	_set(s, 0, tbytes);
 	return (s);
 }
