368bd09222af5639af830fbcf999ecfdeba48646
Ayoub <sb.naba2@gmail.com>
2023-09-07 17:49:49 -0700

    a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -45,6 +45,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	nptr = malloc(new_size);
 	if (nptr == NULL)
 		return (NULL);
+	if (min == new_size)
+		return (ptr);
 	if (new_size < old_size)
 		min = new_size;
 	nptr = derefrence(nptr, ptr, min);
