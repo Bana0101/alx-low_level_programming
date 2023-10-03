44d0512e811dd2bee6e30b17547ad96ee5b45339
Ayoub <sb.naba2@gmail.com>
2023-10-03 05:16:42 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,6 +31,8 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	ssize_t nbyt = 0, len = _strlen(text_content);
 
+	if (!text_content)
+		return (1);
 	if (!filename)
 		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
