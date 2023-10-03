f5c37fd4a3cd97b1da5b6ae99a08920ccc58a404
Ayoub <sb.naba2@gmail.com>
2023-10-02 17:03:21 -0700

    a function that appends text at the end of a file.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int _strlen(char *s)
  * Return: integer
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
 	ssize_t nbyt = 0, len = _strlen(text_content);
