9f35feac5e4aa2f351321c5f00622bc04ef85fb7
Ayoub <sb.naba2@gmail.com>
2023-10-03 07:15:57 -0700

    a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -46,7 +46,7 @@ int main(int ac, char **av)
 	if (b < 1 || b != sizeof(h))
 		dprintf(STDERR_FILENO, NOT_READ, av[1]), exit(98);
 	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
-		       	h.e_ident[3] == 'F')
+			h.e_ident[3] == 'F')
 		printf("ELF header:\n");
 	else
 		dprintf(STDERR_FILENO, "Not ELF file : %s\n", av[1]), exit(98);
