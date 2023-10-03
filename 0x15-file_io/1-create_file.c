1685f22a6909333e8286f749cb983a61a32c58ac
Ayoub <sb.naba2@gmail.com>
2023-10-03 05:47:43 -0700

    a function that creat a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,8 +33,6 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	ssize_t nbyt = 0, len = _strlen(text_content);
 
-	if (text_content == NULL)
-		return (1);
 	if (!filename)
 		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
