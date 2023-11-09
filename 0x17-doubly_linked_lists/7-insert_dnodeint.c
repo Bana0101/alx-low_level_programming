80c12fb52ef8ca0f1c83f897a5bbebbd25c937bd
Ayoub <sb.naba2@gmail.com>
2023-11-09 08:29:51 -0800

    a function that inserts a new node at a given position.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: the head of the list
+ * @idx: the index of the new node
+ * @n: the data of the new node
+ * Return: the address of the new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current, *node;
+	unsigned int i, count = 0;
+
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	current = *h;
+	while (current)
+	{
+		current = current->next;
+		count++;
+	}
+	if (idx < count)
+	{
+		current = *h;
+		for (i = 0; i < idx; i++)
+			current = current->next;
+		node->prev = current->prev;
+		node->next = current;
+		current->prev->next = node;
+		current->prev = node;
+		return (node);
+	}
+	return (NULL);
+}
