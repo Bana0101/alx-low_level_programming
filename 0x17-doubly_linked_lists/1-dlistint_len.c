6364931590c05e396347eae40b46131df14bc9dc
Ayoub <sb.naba2@gmail.com>
2023-11-09 06:57:42 -0800

    a function that returns the number of elements in a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,7 +6,7 @@
  * Return: the number of elements of the list
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *ptr;
 	size_t count = 0;
