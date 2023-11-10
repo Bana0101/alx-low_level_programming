8106358371336a39dff72d72f6c0550729c13f25
Ayoub <sb.naba2@gmail.com>
2023-11-10 14:24:39 -0800

    deletes the node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,8 +12,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current;
 	unsigned int i, count = 0;
 
-	if (!head || !*head)
-		return (-1);
 	current = *head;
 	while (current)
 	{
@@ -26,6 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (index == 0)
 		{
 			*head = (*head)->next;
+			*head->prev = NULL;
 			free(current);
 			return (1);
 		}
