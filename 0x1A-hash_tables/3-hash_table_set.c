2c175559c15943fd3cbba0e952447f38bbcc2140
Ayoub <sb.naba2@gmail.com>
2023-11-30 13:45:11 -0800

    A function that add an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *current, *node;
+	unsigned long int index;
+
+
+	if (!ht || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = (ht->array)[index + 1];
+	ht->array[index] = node;
+	return (1);
+}
+
