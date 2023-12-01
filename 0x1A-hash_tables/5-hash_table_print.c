056d9a14611a402106e584c29578843e0a55e208
Ayoub <sb.naba2@gmail.com>
2023-12-01 13:32:07 -0800

    print the hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,7 +14,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (!ht || !ht->size || !ht->array)
 		return;
 	while (++i < ht->size)
-	
+	{	
 		current = ht->array[i];
 		if (current && !first)
 		{
