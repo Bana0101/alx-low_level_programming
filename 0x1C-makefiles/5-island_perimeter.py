08b73c1fb44697043b6b87a0b7611cb1819a79d7
Ayoub <sb.naba2@gmail.com>
2024-01-25 06:56:16 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,8 +11,8 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 sides += 1
-                if (grid[i][j + 1] == 1 and j < len(grid[0]) - 1):
+                if (j < len(grid[0]) - 1 and grid[i][j + 1] == 1):
                     concat += 1
-                if (grid[i + 1][j] == 1 and i < len(grid) - 1):
+                if (i < len(grid) - 1 and grid[i + 1][j] == 1):
                     concat += 1
     return sides * 4 - concat * 2
